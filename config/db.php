8e8ce416aad674310b42e030f2493e69f8d488cb
k1osha <k1osha@derpy.ru>
2020-12-12 00:28:32 +1000

    project init

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'class'=>'\yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=finance',
+        'username' => 'u0831054_console',
+        'password' =>'turquiose_distribution',
+    ];
